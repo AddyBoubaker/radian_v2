079ca63d0e0c3fb784ae0c3505b6d9ea9e9ebe2a
AddyBoubaker <120931766+AddyBoubaker@users.noreply.github.com>
2022-12-18 13:05:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,48 @@
-# Just draw a star with math and python LOL
 import turtle
-import math
 
-pi = math.pi
-points = [pi*2, pi/2, pi, (4*pi)/3, (5*pi)/3] # points...
-steps = [3, 0, 2, 4, 1, 3] # Here is the cordinates from x & y that will form the star shape
-x = []
-y = []
-i = 0
+# create the turtle object
+flag = turtle.Turtle()
 
-while True:
-    if(i > len(points)-1):
-        break
-    x.append(math.cos(points[i]))
-    y.append(math.sin(points[i]))
-    i += 1
+# set the turtle speed (0 is the fastest, 10 is the slowest)
+flag.speed(0)
 
-t = turtle.Turtle()
-t.down()
-i = 0
-#Doing Moves...
-while i < len(steps):
-    t.goto(x[steps[i]]*50, y[steps[i]]*50)
-    i += 1
-    print(i)
+# set the pen size
+flag.pensize(3)
 
-turtle.mainloop()
+# set the fill color to red
+flag.fillcolor("red")
+
+# move the turtle to the starting position
+flag.penup()
+flag.goto(-200, -200)
+flag.pendown()
+
+# draw the rectangle
+flag.begin_fill()
+for i in range(2):
+    flag.forward(500)
+    flag.left(90)
+    flag.forward(300)
+    flag.left(90)
+flag.end_fill()
+
+# set the fill color to green
+flag.fillcolor("green")
+
+# move the turtle to the starting position
+flag.penup()
+flag.goto(-50, -50)
+flag.pendown()
+
+# draw the triangle
+flag.begin_fill()
+for i in range(5):
+    flag.forward(200)
+    flag.right(144)
+flag.end_fill()
+
+# hide the turtle
+flag.hideturtle()
+
+# keep the window open until it is closed
+turtle.exitonclick()
